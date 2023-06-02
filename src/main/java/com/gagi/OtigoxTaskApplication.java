1993c27315624914785e72e9489deb5538a6191e
bibiki <nberani@hotmail.com>
2023-06-02 15:37:13 +0200

    initializing

---
+++ src/main/java/com/gagi/OtigoxTaskApplication.java
@@ -0,0 +1,13 @@
+package com.gagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtigoxTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtigoxTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gagi/OtigoxTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtigoxTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
