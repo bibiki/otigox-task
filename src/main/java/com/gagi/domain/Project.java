beea116ba1fda99dcfde01664981fb88c69f3e2f
bibiki <nberani@hotmail.com>
2023-06-02 16:37:32 +0200

    adding validation to entity attributes

--- src/main/java/com/gagi/domain/Project.java
+++ src/main/java/com/gagi/domain/Project.java
@@ -2,6 +2,7 @@ package com.gagi.domain;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +17,9 @@ public class Project {
 	private long id;
 	@ManyToMany
 	private List<User> users;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = true)
 	private String description;
 
 	public Long getId() {
--- src/main/java/com/gagi/domain/User.java
+++ src/main/java/com/gagi/domain/User.java
@@ -1,9 +1,11 @@
 package com.gagi.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 
 @Entity(name = "app_users")
 public class User {
@@ -11,7 +13,10 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	@Column(nullable = false)
 	private String name;
+	@Column(unique = true, nullable = false)
+	@Email
 	private String email;
 
 	public Long getId() {
