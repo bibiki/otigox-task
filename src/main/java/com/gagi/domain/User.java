e4c190f0e9bb23c1f7603b45db314ec67152efa9
bibiki <nberani@hotmail.com>
2023-06-03 11:25:05 +0200

    adding a couple of tests

--- src/main/java/com/gagi/domain/User.java
+++ src/main/java/com/gagi/domain/User.java
@@ -1,24 +1,38 @@
 package com.gagi.domain;
 
-import jakarta.persistence.Column;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity(name = "app_users")
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"email"}, name = "Email must be unique")})
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	@Column(nullable = false)
+	@NotEmpty(message = "Name may not be null or empty")
 	private String name;
-	@Column(unique = true, nullable = false)
+	@NotEmpty(message = "Email may not be null or empty")
 	@Email
 	private String email;
 
+	public User() {
+		
+	}
+	
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -41,5 +55,27 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && id == other.id && Objects.equals(name, other.name);
 	}
 }
--- src/test/java/com/gagi/OtigoxTaskApplicationTests.java
+++ src/test/java/com/gagi/OtigoxTaskApplicationTests.java
@@ -1,13 +1,61 @@
 package com.gagi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.gagi.domain.User;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebClient
 class OtigoxTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient testClient;
 
+	@Test
+	void shouldCreateUserAndThenRetrieveTheSameUser() {
+		User user = new User("Ngadhnjim", "unique@hotmail.com");
+		testClient.post().uri("/users").body(Mono.just(user), User.class)
+		.exchange().expectStatus().isCreated()
+		.expectHeader().contentType(MediaTypes.HAL_JSON)
+		.expectBody()
+		.jsonPath("$.name").isEqualTo(user.getName())
+		.jsonPath("$.email").isEqualTo(user.getEmail())
+		.jsonPath("$._links.user.href").isNotEmpty()
+		.jsonPath("$._links.user.href")
+		.value((v)-> {
+			testClient.get().uri(String.valueOf(v))
+			.exchange().expectStatus().isOk()
+			.expectHeader().contentType(MediaTypes.HAL_JSON)
+			.expectBody()
+			.jsonPath("$.name").isEqualTo(user.getName())
+			.jsonPath("$.email").isEqualTo(user.getEmail());
+		});
+		
+    }
+	
+	@Test
+	void shouldTryButFailToAddADifferentUserWithTheSameEmailAsAnotherUser() {
+		User user = new User("Ngadhnjim", "notunique@hotmail.com");
+		testClient.post().uri("/users").body(Mono.just(user), User.class)
+		.exchange().expectStatus().isCreated();
+		
+		User otherUser = new User("Berani", "notunique@hotmail.com");
+		testClient.post().uri("/users").body(Mono.just(otherUser), User.class)
+		.exchange().expectStatus().is4xxClientError()
+		.expectBody()
+		.jsonPath("$.message").isNotEmpty()
+		.jsonPath("$.message").value((v) -> {
+			assertTrue(String.valueOf(v).contains("Email must be unique"));
+		});
+	}
 }
