d87cd948e2a39352884a038b0586567c2174b1c9
bibiki <nberani@hotmail.com>
2023-06-03 06:58:08 +0200

    adding search repository methods

--- src/main/java/com/gagi/repository/ProjectRepository.java
+++ src/main/java/com/gagi/repository/ProjectRepository.java
@@ -1,10 +1,14 @@
 package com.gagi.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.gagi.domain.Project;
 
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long>, CrudRepository<Project, Long>{
 
+	List<Project> findByName(@Param("name") String name);
 }
--- src/main/java/com/gagi/repository/UserRepository.java
+++ src/main/java/com/gagi/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.gagi.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.gagi.domain.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long>, CrudRepository<User, Long>{
 
+	List<User> findByNameAndEmail(@Param("name") String name, @Param("email") String email);
 }
